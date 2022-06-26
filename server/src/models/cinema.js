3295d3c68173c5554776432c57a403b0150ec96b
namtrungdao <trungnamdao@gmail.com>
2022-06-26 16:23:36 +0700

    Fix cinema models - server
    Fix cinema models - server

--- server/src/models/cinema.js
+++ server/src/models/cinema.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const feedback = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+  },
+  comment: {
+    type: String,
+  },
+});
+
 const cinemaSchema = new Schema({
   name: {
     type: String,
@@ -16,13 +28,13 @@ const cinemaSchema = new Schema({
     type: String,
     required: true,
     trim: true,
-    lowercase: true,
+    // lowercase: true,
   },
   district: {
     type: String,
     required: true,
     trim: true,
-    lowercase: true,
+    // lowercase: true,
   },
   seats: {
     type: [Schema.Types.Mixed],
@@ -35,7 +47,25 @@ const cinemaSchema = new Schema({
   image: {
     type: String,
   },
+  contact: {
+    type: String,
+    required: true,
+    trim: true,
+    // lowercase: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+    // lowercase: true,
+  },
+  feedbacks: {
+    type: [feedback],
+    required: false
+  }
 });
+
+
 
 const Cinema = mongoose.model('Cinema', cinemaSchema);
 
