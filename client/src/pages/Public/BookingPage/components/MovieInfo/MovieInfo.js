b0f5d4da0497826347aa458b37b1657d384a5597
longdinh1109 <nglongd@gmail.com>
2022-07-30 10:24:33 +0700

    update css

--- client/src/pages/Public/BookingPage/components/MovieInfo/MovieInfo.js
+++ client/src/pages/Public/BookingPage/components/MovieInfo/MovieInfo.js
@@ -5,7 +5,7 @@ const useStyles = makeStyles(theme => ({
   movieInfos: {
     background: 'rgba(57, 61, 67, 0.5)',
     position: 'relative',
-    height: '100%'
+    height: '500px'
   },
   background: {
     position: 'absolute',
